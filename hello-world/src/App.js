e5f8dd7521ef02ab67e92e6dec00ef60cb5dd58f
Brian Stoots <bstoots@gmail.com>
2017-01-01 14:03:45 -0500

    Working tic-tac-toe from React tutorial.  Nifty.

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,6 +1,103 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './tictactoe.css';
+
+class HorizontalRule extends Component {
+  render() {
+    return (
+      <hr/>
+    );
+  }
+}
+
+// class Square extends Component {
+//   render() {
+//     return(
+//       <button className="square" onClick={() => this.props.onClick()}>{this.props.value}</button>
+//     );
+//   }
+// }
+
+// Since Square is now stateless we can use this simplified syntax, neat!
+function Square(props) {
+  return (
+    <button className="square" onClick={() => props.onClick()}>{props.value}</button>
+  );
+}
+
+class Board extends Component {
+  constructor() {
+    super();
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true
+    };
+  }
+  renderSquare(i) {
+    return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
+  }
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.playerSymbol();
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext
+    });
+  }
+  playerSymbol() {
+    return this.state.xIsNext ? 'X' : 'O';
+  }
+  render() {
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    }
+    else {
+      status = 'Next player: ' + this.playerSymbol();
+    }
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends Component {
+  render() {
+    return(
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
 
 class App extends Component {
   render() {
@@ -13,9 +110,34 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <HorizontalRule/>
+
+        <Game/>
+
       </div>
     );
   }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 export default App;
